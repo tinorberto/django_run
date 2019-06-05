7347a766af64b6a43522e50d50f19581cf3e3d67
Tiago da Costa Norberto <tiago.cnorberto@pbh.gov.br>
2019-06-05 08:11:11 -0300

    Core

---
+++ django_run/run_monitor/core/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+class Run(models.Model):
+    id_run = models.AutoField(primary_key=True)
+    run_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField( default=timezone.now)
+    total_time = models.CharField(max_length=200)
+    distance = models.FloatField()
--- django_run/run_monitor/core/urls.py
+++ django_run/run_monitor/core/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
+from . import views
 
 urlpatterns = [
-    path('', include('core.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home')
 ]
