5d34eb325aa611ab13817e6f8a7c83ac88bc9d30
Tiago da Costa Norberto <tiago.cnorberto@pbh.gov.br>
2019-06-26 08:17:07 -0300

    Validate form

--- django_run/run_monitor/core/forms.py
+++ django_run/run_monitor/core/forms.py
@@ -4,10 +4,11 @@ from .models import Run
 from datetime import datetime
 
 class RunForm(forms.ModelForm):
-    created_dateField = forms.DateTimeField(initial=datetime.now(), label = 'Data da corrida',
+    created_dateField = forms.DateTimeField(
+    initial=datetime.now().strftime('%m-%d-%y %H:%M'), label = 'Data da corrida',
     widget = forms.TextInput( attrs={'class':'form-control'}))
     
-    run_dateField = forms.DateTimeField(initial=datetime.now(), label = 'Data do registro ',
+    run_dateField = forms.DateTimeField(initial=datetime.now().strftime('%m-%d-%y %H:%M'), label = 'Data do registro ',
     widget = forms.TextInput( attrs={'class':'form-control'}))
 
     time_dateField = forms.TimeField(label = 'Tempo total :',
@@ -19,13 +20,3 @@ class RunForm(forms.ModelForm):
     class Meta:
         model = Run
         fields = ['created_dateField', 'run_dateField', 'time_dateField', 'distance_Field']
-
-
-
-"""
-    id_run = models.AutoField(primary_key=True)
-    run_date = models.DateTimeField(default=timezone.now)
-    created_date = models.DateTimeField( default=timezone.now)
-    total_time = models.CharField(max_length=200)
-    distance = models.FloatField()
-"""
--- django_run/run_monitor/core/views.py
+++ django_run/run_monitor/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib import messages
 from django.views.generic import ListView, DetailView 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Run
@@ -23,6 +23,22 @@ class RunCreate (CreateView):
     def get_success_url(self):
        return reverse('run_list')
 
+    def form_invalid(self,form):
+        # Add action to invalid form phase
+        print ("-------")
+        messages.success(self.request, 'An error occured while processing the payment')
+        return self.render_to_response(self.get_context_data(form=form)) 
+
+    def form_valid(self, form):
+        print ('-OOOOOOOOOOOOOO--')
+        print (model.created_date)
+        model.created_date = model.created_date.strftime("%YYYY-%MM-%DD %HH:%MM:%SS.SSS")
+        print (model.created_date)
+        model = form.save(commit=False)
+        model.submitted_by = self.request.user
+        model.save()
+        return HttpResponseRedirect(self.get_success_url())
+
 class RunView(DetailView):
     model = Run
 
@@ -45,3 +61,13 @@ class RunStepsCreate (CreateView):
     fields = '__all__'
     def get_success_url(self):
        return reverse('run_list')
+    
+    def form_valid(self, form):
+        pirnt ('---')
+        print (model.created_date)
+        model.created_date = model.created_date.strftime("%YYYY-%MM-%DD %HH:%MM:%SS.SSS")
+        print (model.created_date)
+        model = form.save(commit=False)
+        model.submitted_by = self.request.user
+        model.save()
+        return HttpResponseRedirect(self.get_success_url())
