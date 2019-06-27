04e4ab8596af4aac632adffbdaa9d0b129d16f42
Tiago da Costa Norberto <tiago.cnorberto@pbh.gov.br>
2019-06-27 15:17:57 -0300

    Forms

--- django_run/run_monitor/core/forms.py
+++ django_run/run_monitor/core/forms.py
@@ -4,19 +4,25 @@ from .models import Run
 from datetime import datetime
 
 class RunForm(forms.ModelForm):
-    created_dateField = forms.DateTimeField(
+    created_date = forms.DateTimeField(
     initial=datetime.now().strftime('%m-%d-%y %H:%M'), label = 'Data da corrida',
     widget = forms.TextInput( attrs={'class':'form-control'}))
     
-    run_dateField = forms.DateTimeField(initial=datetime.now().strftime('%m-%d-%y %H:%M'), label = 'Data do registro ',
+    run_date = forms.DateTimeField(initial=datetime.now().strftime('%m-%d-%y %H:%M'), label = 'Data do registro ',
     widget = forms.TextInput( attrs={'class':'form-control'}))
 
-    time_dateField = forms.TimeField(label = 'Tempo total :',
+    total_time = forms.TimeField(label = 'Tempo total :',
     widget = forms.TextInput( attrs={'class':'form-control'}))
 
-    distance_Field = forms.FloatField(label = 'Distancia total :',
+    distance = forms.FloatField(label = 'Distancia total :',
     widget = forms.TextInput( attrs={'class':'form-control'}))
 
     class Meta:
         model = Run
-        fields = ['created_dateField', 'run_dateField', 'time_dateField', 'distance_Field']
+        fields = ['created_date', 'run_date', 'total_time', 'distance']
+
+    def clean_distance(self): 
+        print (self.cleaned_data['distance'])
+        data = 5
+        self.cleaned_data['distance'] = 2
+        return  self.cleaned_data['distance']
